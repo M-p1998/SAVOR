215863f587c9992e5e544d491253b77b6747e25d
M_p <skyusmpp@gmail.com>
2022-07-05 23:26:55 -0700

    updates

--- recipe_app/models/recipe.py
+++ recipe_app/models/recipe.py
@@ -1,10 +1,12 @@
-
-
+from 
 class Recipe:
     def __init__(self,data):
+        self.id=data["id"]
         self.name=data["name"]
         self.description = data["description"]
-        self.instructions= data["instructions"]
+        self.instruction= data["instruction"]
         self.date_made_on = data["date_made_on"]
         self.under_30_minutes = data["under_30_minutes"]
-        
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
